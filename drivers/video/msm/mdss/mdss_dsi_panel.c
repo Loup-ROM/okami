182fb24773f1a0fb81c2c505840390a8bc98d212
bitrvmpd <e.noyer.silva@gmail.com>
2020-07-07 18:49:36 -0500

    :sparkles: mdss: Add state_notifier hooks for santoni

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -28,6 +28,9 @@
 #ifdef TARGET_HW_MDSS_HDMI
 #include "mdss_dba_utils.h"
 #endif
+#ifdef CONFIG_STATE_NOTIFIER
+#include <linux/state_notifier.h>
+#endif
 #define DT_CMD_HDR 6
 #define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
@@ -873,6 +876,10 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 
 	pr_debug("%s: ndx=%d\n", __func__, ctrl->ndx);
 
+#ifdef CONFIG_STATE_NOTIFIER
+	state_resume();
+#endif
+
 	if (pinfo->dcs_cmd_by_left) {
 		if (ctrl->ndx != DSI_CTRL_LEFT)
 			goto end;
@@ -989,6 +996,10 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 
 	pr_debug("%s: ctrl=%pK ndx=%d\n", __func__, ctrl, ctrl->ndx);
 
+#ifdef CONFIG_STATE_NOTIFIER
+	state_suspend();
+#endif
+
 	if (pinfo->dcs_cmd_by_left) {
 		if (ctrl->ndx != DSI_CTRL_LEFT)
 			goto end;
@@ -1030,6 +1041,10 @@ static int mdss_dsi_panel_low_power_config(struct mdss_panel_data *pdata,
 		mdss_dsi_panel_set_idle_mode(pdata, true);
 	else
 		mdss_dsi_panel_set_idle_mode(pdata, false);
+#ifdef CONFIG_STATE_NOTIFIER
+	if (enable)
+		state_suspend();
+#endif
 	pr_debug("%s:-\n", __func__);
 	return 0;
 }
